93d837bb83720ddfea8910a8eb5010c25d64529f
Eduardo Chaves <educostachaves@hotmail.com>
2016-01-10 15:00:57 -0200

    set header component to works with react-route

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+module.exports = React.createClass({
+  render: function() {
+    return <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Imgur Browser
+        </Link>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <a href="#">
+              Topic 1
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  }
+});
