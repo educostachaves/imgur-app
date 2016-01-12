4a8c11c38c54f60845ca4a074c9e352c70e9c9e2
Eduardo Chaves <educostachaves@hotmail.com>
2016-01-12 20:05:34 -0200

    build image store

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,7 +1,28 @@
 var React = require('react');
+var Actions = require('../actions');
+var ImageStore = require('../stores/topic-store');
+var Reflux = require('reflux');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      images: []
+    }
+  },
+  componentWillMount: function() {
+    Actions.getImages(this.props.params.id);
+  },
   render: function() {
-    return <div>Topic</div>
+    return <div>
+
+    </div>
+  },
+  onChange: function() {
+    this.setState({
+      images: images
+    })
   }
 })
