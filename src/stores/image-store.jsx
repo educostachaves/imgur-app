80201535e0f5ae3f58ee5760918ecdc86ced8d3d
Eduardo Chaves <educostachaves@hotmail.com>
2016-01-13 00:59:45 -0200

    filter image data

--- src/stores/image-store.jsx
+++ src/stores/image-store.jsx
@@ -1,17 +1,21 @@
-var Api = require('../utils/api');
 var Reflux = require('reflux');
+var Api = require('../utils/api');
 var Actions = require('../actions');
+var _ = require('lodash');
 
 module.exports = Reflux.createStore({
   listenables: [Actions],
-  getImages: function(topicId) {
-    return Api.get('topics/' + topicId)
+  getImages: function(topicId){
+    Api.get('topics/' + topicId)
       .then(function(json){
-        this.images = json.data;
+        this.images = _.reject(json.data, function(image) {
+          return image.is_album
+        });
+
         this.triggerChange();
       }.bind(this));
   },
   triggerChange: function() {
-    this.trigger('change',this.images);
+    this.trigger('change', this.images);
   }
 });
